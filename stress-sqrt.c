1b93f7a91d96d4d2b1761d8aa33d23f106f6dd67
mlryckman27 <mlryckman27@gmail.com>
2023-04-26 20:58:14 -0400

    Frontend allows for testing individual CPUs by letting user enter CPU number; exit command now kills all child process before exiting

--- stress-sqrt.c
+++ stress-sqrt.c
@@ -1,44 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 
-char* cpu[] = {NULL, NULL};
-
-
 int main(int argc, char** argv) {
 
-    int status;
-
-    pid_t pid;
-    char cpu[3];
+    char* prompt[] = {"Enter CPU number to start, 'exit' to quit: "};
 
     while (1) {
-        fgets(cpu, 3, stdin);
-        int cpuArg = atoi(cpu);
 
-        pid = fork();
+        // Get user input
+        char workerArgs[100];
+        printf("%s", prompt[0]);
+        fgets(workerArgs, 100, stdin);
+        workerArgs[strcspn(workerArgs, "\n")] = '\0';
+
+        // Exit application if 'exit' is specified
+        if (strcmp(workerArgs, "exit") == 0) {
+            printf("Exiting...\n");
+            kill(0, SIGTERM);
+            exit(0);
+        }
+
+        // Fork child process
+        pid_t pid = fork();
+
+        // Handle behavior for parent, child, or failure to fork child
         if (pid < 0) {
             printf("Fork failed: %d\n", pid);
             exit(1);
         }
         else if (pid == 0) {
-            char** args;
-            args[0] = (char*) malloc(8 * sizeof(char));
-            args[1] = (char*) malloc(3 * sizeof(char));
+            // This is the child process, a worker will execute from here
+            char* args[] = {"worker", workerArgs, NULL};
 
-            args[0] = "./worker";
-            args[1] = cpu;
-            printf("%s\n", args[0]);
-            printf("%s\n", args[1]);
-            //execv(args[0], args);
-            //printf("CPU to test: %d\n", cpuArg);
+            int ret = execv("./worker", args);
+
+            if (ret != 0) {
+                printf("Failed to execute %s: %d\n", args[0], ret);
+                exit(1);
+            }
         }
         else {
+            // This is the parent process, it will continue to accept args
+            
+            // Status sent back from child process
             int status;
-            waitpid(pid, &status, 0);
+
+            while(waitpid(pid, &status, WNOHANG) == 0) {
+                // Get user input again
+                char newWorkerArgs[100];
+                printf("%s", prompt[0]);
+                fgets(newWorkerArgs, 100, stdin);
+                newWorkerArgs[strcspn(newWorkerArgs, "\n")] = '\0';
+
+                // Exit application if 'exit' is specified
+                if (strcmp(newWorkerArgs, "exit") == 0) {
+                    printf("Exiting...\n");
+                    kill(0, SIGTERM);
+                    exit(0);
+                }
+
+                // Fork another child process
+                pid_t newPid = fork();
+
+                if (newPid < 0) {
+                    printf("Fork failed: %d\n", pid);
+                    exit(1); 
+                }
+                else if (newPid == 0) {
+                    char* newargs[] = {"worker", newWorkerArgs, NULL};
+                    
+                    int ret = execv("./worker", newargs);
+
+                    if (ret != 0) {
+                        printf("Failed to execute %s: %d\n", newargs[0], ret);
+                        exit(1);
+                    }
+                }
+                else {
+                    // Set the pid from the outer loop to the pid from the new fork
+                    pid = newPid;
+                }
+            }
+    
         }
     }
 
@@ -50,6 +98,11 @@ int main(int argc, char** argv) {
 
 /**
  * TODO:
- * 	- fork process for each CPU specified and run sqrt on each individual process
- *  - from main process, allow user to send stop signal to each child process to stop the stress test
+ * 	- print information about number of available CPUs
+ *  - implement option for logging results of calculations done on a particular CPU and time run
+ *  
+ * 
+ *
+ *
+ *
 */
--- worker.c
+++ worker.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
     }
 
     int cpu = atoi(argv[1]);
-    printf("Setting up worker on CPU %d\n", cpu);
+    //printf("Setting up worker on CPU %d", cpu);
 
     // Setup CPU mask
     cpu_set_t set;
@@ -34,49 +34,3 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
-// #define _GNU_SOURCE
-
-
-// #include <sched.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <sys/wait.h>
-// #include <time.h>
-// #include <math.h>
-
-
-// int main (int argc, char **argv) {
-
-// 	// printf("num args: %d\n", argc);
-
-// 	// int* cpuArray = (int*) malloc((argc - 1) * sizeof(int));
-// 	// for (int i = 1; i < argc; i++)
-// 	// 	cpuArray[i] = atoi(argv[i]);
-
-
-// 	printf("%s\n", argv[1]);
-
-// 	// Seed random number generator
-// 	srand(time(NULL));
-
-// 	// Setup CPU mask
-// 	cpu_set_t set;
-// 	CPU_ZERO(&set);
-
-// 	// set CPU to test
-// 	int cpu = atoi(argv[1]);
-// 	printf("%d\n", cpu);
-
-// 	CPU_SET(cpu, &set);
-
-// 	// Infinite loop over square root of random number
-// 	if (sched_setaffinity(0, sizeof(set), &set) == 0) {
-// 		while (1) {
-// 			sqrt(rand());
-// 		}
-// 	}
-
-// 	return 0;
-// }
